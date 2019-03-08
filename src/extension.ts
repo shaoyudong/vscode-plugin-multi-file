09f5a5039deb3873b3b0c97bbf65b9ea763262c0
yudong.shao <yudong.shao@qunar.com>
2019-03-09 01:07:25 +0800

    feat: first commit

---
+++ src/extension.ts
@@ -0,0 +1,28 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import * as path from 'path';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	vscode.commands.registerCommand('extension.multifile', (e) => {
+		vscode.window.showInformationMessage('change!!!');
+		const folders: any = vscode.workspace.workspaceFolders || [];
+		const activeTextEditor: any = vscode.window.activeTextEditor;
+		const workspacePath: string = folders[0].uri.path;
+		const activePath: string = activeTextEditor.document.fileName;
+		const relativePath: string = path.relative(workspacePath, activePath);
+		if (!relativePath.match(/^source/)) {
+			return;
+		}
+		vscode.workspace.openTextDocument(path.resolve(workspacePath, relativePath.replace(/^source/, 'dist'))).then(document => {
+			console.log('hahaha');
+			vscode.window.showTextDocument(document, vscode.ViewColumn.Beside, true)
+		});
+	})
+	
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
